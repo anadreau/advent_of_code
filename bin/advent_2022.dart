f1320eb51ee4ccb34880f69e1e5ccad6b141d2b4
Alex Nadreau <anadreau42@gmail.com>
2022-12-01 15:02:03 -0700

    cleaned up

--- bin/advent_2022.dart
+++ bin/advent_2022.dart
@@ -1,7 +1,8 @@
 import 'package:advent_2022/advent_2022.dart' as advent_2022;
 
 void main() async {
-  advent_2022.sortElf();
-  advent_2022.topThreeCalories();
-  // print('Hello world: ${advent_2022.calculate()}!');
+  var topElf = await advent_2022.sortElf();
+  var threeElvesTotal = await advent_2022.topThreeCalories();
+  print('Elf carrying most callories is carrying: ${topElf.calories}');
+  print('Top Three elves total calories equal: $threeElvesTotal');
 }
--- lib/advent_2022.dart
+++ lib/advent_2022.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:developer';
-
 import 'dart:io' as io;
 import 'package:advent_2022/elf_model.dart';
 
@@ -11,20 +10,23 @@ Future<List<Elf>> _generateList() async {
   int elfId = 1;
   final file = io.File('lib/data/elfs.txt');
 
+  //Read elfText.txt file by line
   Stream<String> lines =
       file.openRead().transform(utf8.decoder).transform(LineSplitter());
   try {
     await for (var line in lines) {
-      //log(line);
       Elf elf = Elf(id: elfId);
+      //add line to item list until there is an empty line
       if (line.isNotEmpty) {
         items.add(int.parse(line));
       }
+      //Once empty line is reached add items, calculate calories
+      //add to elf object and increment to next elf
       if (line.isEmpty) {
         elf.items = items;
         elf.calories = await _calculateCalories(elf);
         elfList.add(elf);
-        //log('elf: ${elf.id}, \nitems: ${elf.items}, \nCalories: ${elf.calories}');
+        //clear items list for next elf
         items.clear();
         elfId++;
       }
@@ -32,10 +34,11 @@ Future<List<Elf>> _generateList() async {
   } catch (e) {
     log('$e');
   }
-  log('${elfList.length}');
+  //log('${elfList.length}');
   return elfList;
 }
 
+//Iterate through Elf's Items and add them to total Calories
 Future<int> _calculateCalories(Elf elf) async {
   int totalCalories = 0;
 
@@ -46,17 +49,18 @@ Future<int> _calculateCalories(Elf elf) async {
   return totalCalories;
 }
 
+//Sort list of elves by largest amount of calories in Items
 Future<Elf> sortElf() async {
   List<Elf> sortedElfs = await _generateList();
-  log('${sortedElfs.first.id} and ${sortedElfs.first.calories}');
+  //log('${sortedElfs.first.id} and ${sortedElfs.first.calories}');
   sortedElfs.sort(((a, b) {
     return a.calories.compareTo(b.calories);
   }));
   var reversedList = sortedElfs.reversed;
   for (Elf elf in reversedList) {
-    log('Elf ID: ${elf.id} with total calories: ${elf.calories}');
+    //log('Elf ID: ${elf.id} with total calories: ${elf.calories}');
   }
-  log('${sortedElfs.first.id} and ${sortedElfs.first.calories}');
+  //log('${sortedElfs.first.id} and ${sortedElfs.first.calories}');
   return reversedList.first;
 }
 
@@ -71,6 +75,6 @@ Future<int> topThreeCalories() async {
       reversedList.elementAt(1).calories +
       reversedList.elementAt(2).calories;
 
-  log('Top three calories equal: $topThree');
+  //log('Top three calories equal: $topThree');
   return topThree;
 }
