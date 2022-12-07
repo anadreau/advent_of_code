3b5041996587f38ff9148030d752db4aa3fd4b68
anadreau <anadreau42@gmail.com>
2022-12-06 22:25:03 -0700

    Day6 in progress

--- lib/day6/tuning_trouble.dart
+++ lib/day6/tuning_trouble.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:io' as io;
-import 'dart:collection';
 
 //day 6
 final file = io.File('lib/data/packet_marker.txt');
 int marker = 0;
 String markerLetter = '';
 int elementCount = 1;
+int code = 0;
+int search1 = 0;
+int search2 = 0;
+int search3 = 0;
 
 Future<int> packetFinder() async {
   //marker value
@@ -19,23 +22,29 @@ Future<int> packetFinder() async {
       log('line Length ${line.length}');
       for (int i = 0; i < line.length - 4; i++) {
         String workingLine = line.substring(i, i + 4);
-        List list = workingLine.split('');
 
-        //log('$list');
-        for (int y = 0; y < list.length; y++) {
-          for (int z = 0; z < list.length; z++) {
-            if (list[z] == list[y] && z != y) {
-              markerLetter = list[z];
-              //log('failed: $markerLetter');
-              break;
-            }
-            if (list[z] == list.length - 1 && list[y] == list.length - 1) {
-              marker = i + 4;
-              log('$marker');
+        log(workingLine);
+
+        for (var x = 0; x < workingLine.length; x++) {
+          for (var y = 0 + 1; y < workingLine.length; y++) {
+            var search = workingLine.indexOf(workingLine[x], y);
+            log('$search');
+            if (x = 1 && search == -1) {
+              code = i + 4;
+              log('Code is $code');
               break;
             }
           }
         }
+
+        // for (var a = 0; a < list.length; a++) {
+        //   for (var b = a + 1; b < list.length; b++) {
+        //     var search = list.indexOf(list[a]);
+
+        //     var searchResults = list.indexOf(list[b], search);
+        //     log('$searchResults');
+        //   }
+        // }
       }
     }
   } catch (e) {
