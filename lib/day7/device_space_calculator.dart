7fbcace79d3649319281ade14bd66fdc7cb8e76d
anadreau <anadreau42@gmail.com>
2022-12-09 21:54:03 -0700

    Day7 starting

--- lib/day7/device_space_calculator.dart
+++ lib/day7/device_space_calculator.dart
@@ -1,3 +1,9 @@
-
+import 'dart:convert';
+import 'dart:io' as io;
 
 //day 7
+var file = io.File('lib/data/space_on_device.txt');
+
+void spaceCalculator() {
+  var lines = file.openRead().transform(utf8.decoder).transform(LineSplitter());
+}
