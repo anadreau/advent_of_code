d1b403612c90ccbe9c6eaab3f2ba1a3b6357e70a
Alex Nadreau <anadreau42@gmail.com>
2023-12-08 10:18:42 -0700

    Starting day 2 part 1

---
+++ lib/2023/day2/elf_game_calc.dart
@@ -0,0 +1,18 @@
+
+//Parse input
+
+//split line
+
+//capture game number
+
+//capture blue count and find total
+
+//capture red count and find total
+
+//capture green count and find total
+
+//compare total to game threshhold
+
+//if all colors are under threshhold than add game number to sum
+
+//return sum of all passing gameID as answer
---
+++ lib/2023/day2/game_model.dart
@@ -0,0 +1,13 @@
+class ElfGame {
+  final int gameID;
+  final int blueCount;
+  final int redCount;
+  final int greenCount;
+
+  ElfGame({
+    required this.gameID,
+    this.blueCount = 0,
+    this.greenCount = 0,
+    this.redCount = 0,
+  });
+}
